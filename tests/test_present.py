2a58e7b3b3f5abbe345fc741334c2b1255173960
Ollie Jeffcote <oliverjeffcote@gmail.com>
2025-10-06 17:29:17 +0100

    present class and tests added

---
+++ tests/test_present.py
@@ -0,0 +1,26 @@
+from lib.present import *
+import pytest
+
+def test_wrap_one_present():
+    present = Present()
+    present.wrap("Bike")
+    assert present.contents == "Bike", "Should return Bike as the wrapped present"
+
+def test_try_to_wrap_more_than_one_present():
+    present = Present()
+    present.wrap("New Bike")
+    with pytest.raises(Exception) as err:
+        present.wrap("Scooter")
+    assert str(err.value) == "A contents has already been wrapped."
+
+def test_unwrap_one_existing_present():
+    present = Present()
+    present.wrap("Bike")
+    present.unwrap()
+    assert present.contents == "Bike", "Should return Bike as the unwrapped present"
+
+def test_unwrap_with_no_wrapped_presents():
+    present = Present()
+    with pytest.raises(Exception) as err:
+        present.unwrap()
+    assert str(err.value) == "No contents have been wrapped."
